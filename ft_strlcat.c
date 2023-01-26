b0c622790fb87f23580c44b07a1b62e66ee641c1
kohshi54 <kohshi54.yam@gmail.com>
2023-01-27 00:56:53 +0900

    delete restrict

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,7 +1,7 @@
 #include <stddef.h>
 #include "libft.h"
 
-size_t ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t count;
 
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,7 +1,7 @@
 #include <stddef.h>
 #include "libft.h"
 
-size_t ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	int count;
 
