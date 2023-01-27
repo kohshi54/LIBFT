750912e89ca5e32e0bdb08e238b856f027d7ecfd
kohshi54 <kohshi54.yam@gmail.com>
2023-01-27 14:17:37 +0900

    Makefile

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,46 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyamaguc <kyamaguc@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/27 12:37:11 by kyamaguc          #+#    #+#             */
+/*   Updated: 2023/01/27 12:56:41 by kyamaguc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
-#include <ctype.h>
 #include <stddef.h>
 #include <limits.h>
-#include <stdio.h>
 #include <stdbool.h>
 
-int ft_atoi(const char *str)
+static bool	overflow_min_checker(long num, const char *str)
 {
-	size_t flg;
-	long num;
+	if (num > (LONG_MIN / 10) * -1)
+		return (1);
+	else if (num == ((LONG_MIN / 10) * -1) \
+				&& (*str - '0') > ((LONG_MIN % 10) * -1))
+		return (1);
+	return (0);
+}
 
-	flg = 0;
+static bool	overflow_max_checker(long num, const char *str)
+{
+	if (num > (LONG_MAX / 10))
+		return (1);
+	else if (num == (LONG_MAX / 10) && (*str - '0') > (LONG_MAX % 10))
+		return (1);
+	return (0);
+}
+
+// static char overflow_checker(long num, const char *str, char neg_flg)
+// {
+// 	if (neg_flg == -1)
+// 		if (overflow_min_checker(num, str))
+			
+// 	return (0);
+// }
+
+// check the output of original function later.
+static int	ft_isspace(int c)
+{
+	if ((c == '\t') || (c == '\n') || (c == '\v') \
+					|| (c == '\f') || (c == '\r') || (c == ' '))
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	char	neg_flg;
+	long	num;
+
+	neg_flg = 1;
 	num = 0;
-	while (isspace(*str))
+	while (ft_isspace(*str))
 		str++;
 	if (*str == '-' || *str == '+')
+		if (*str++ == '-')
+			neg_flg = -1;
+	while (ft_isdigit(*str))
 	{
-		if (*str == '-')
-			flg = 1;
-		str++;
-	}
-	while (isdigit(*str))
-	{
-		if (flg)
+		if (neg_flg == -1)
 		{
-			if (num > (LONG_MIN/10) * -1)
-				return ((int)LONG_MIN);
-			else if (num == ((LONG_MIN/10) * -1) && (*str - '0') > (LONG_MIN%10)*-1)
+			if (overflow_min_checker(num, str))
 				return ((int)LONG_MIN);
 		}
 		else
 		{
-			if (num > (LONG_MAX/10))
-				return ((int)LONG_MAX);
-			else if (num == (LONG_MAX/10) && (*str - '0') > (LONG_MAX%10))
+			if (overflow_max_checker(num, str))
 				return ((int)LONG_MAX);
 		}
-		num = (num * 10) + (*str - '0');
-		str++;
+		num = (num * 10) + (*str++ - '0');
 	}
-	if (flg)
-		return ((int)num * (-1));
-	else
-		return ((int)num);
+	return ((int)(num * neg_flg));
 }
